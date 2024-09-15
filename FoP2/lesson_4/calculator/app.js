d06cb13ca1c6949b6dd0c579a2337d5a98bd8179
daryle <daryle.tan@gmail.com>
2024-09-15 11:59:46 -0500

    calculator fin

--- FoP2/lesson_4/calculator/app.js
+++ FoP2/lesson_4/calculator/app.js
@@ -1,13 +1,11 @@
 let total = 0
-let numHolder = []
+let subtotal = 0
 let numStrHolder = []
-let numStr
-let numToAdd
+let currentDisplay = ""
 let isAdding = false
 let isSubtracting = false
 let isMultiplying = false
 let isDividing = false
-// let numOne = document.querySelector(".one").innerText
 
 document.querySelector(".one").addEventListener("click", displayValue)
 document.querySelector(".two").addEventListener("click", displayValue)
@@ -22,37 +20,100 @@ document.querySelector(".zero").addEventListener("click", displayValue)
 
 document.querySelector(".clear").addEventListener("click", makeZero)
 document.querySelector(".add").addEventListener("click", add)
+document.querySelector(".subtract").addEventListener("click", subtract)
+document.querySelector(".multiply").addEventListener("click", multiply)
+document.querySelector(".divide").addEventListener("click", divide)
+document.querySelector(".equal").addEventListener("click", equalNum)
+
+function equalNum() {
+  numStrHolder.push(parseInt(currentDisplay))
+
+  for (let i = 0; i < numStrHolder.length; i++) {
+    if (isAdding) {
+      total += numStrHolder[i]
+      console.log(total, "total")
+    } else if (isSubtracting) {
+      if (i === 0) {
+        total += numStrHolder[0]
+      } else {
+        total -= numStrHolder[i]
+      }
+    } else if (isMultiplying) {
+      if (total === 0) {
+        total = 1
+      }
+      total *= numStrHolder[i]
+      console.log(total, "total")
+    } else if (isDividing) {
+      if (total === 0 && i == 0) {
+        total = numStrHolder[0]
+      } else {
+        total /= numStrHolder[i]
+      }
+    }
+  }
+  document.querySelector("#placeToPutResult").innerText = total
+
+  isAdding = false
+  isSubtracting = false
+  isMultiplying = false
+  isDividing = false
+  currentDisplay = ""
+  numStrHolder = []
+}
 
 function add() {
-  //   document.querySelector("#placeToPutResult").innerText = 0
+  numStrHolder.push(parseInt(currentDisplay))
   isAdding = true
-  numStrHolder.push(numStr)
-  console.log(numStrHolder, "numStrHolder")
-}
-
-function displayValue() {
-  let value = this.innerText
-  if (!isAdding) {
-    numHolder.push(value)
-    numStr = parseInt(numHolder.join(""))
-    document.querySelector("#placeToPutResult").innerText = numStr
-    console.log(typeof numStr, numStr)
-  } else {
-    numHolder.push(value)
-    console.log(numHolder, "numHolder")
-    numStr = parseInt(numHolder.join(""))
-    document.querySelector("#placeToPutResult").innerText = numStr
-    console.log(typeof numStr, numStr)
-  }
-}
-// clears out the results and sets to 0
-function makeZero() {
-  numHolder = []
-  document.querySelector("#placeToPutResult").innerText = numHolder.length
-  console.log("makezero")
+  isSubtracting = false
+  currentDisplay = ""
+  document.querySelector("#placeToPutResult").innerText = 0
+  console.log(numStrHolder, "numStrHolder", isAdding, typeof numStrHolder[0])
 }
 
 function subtract() {
-  total = total + 9
-  document.querySelector("#placeToPutResult").innerHTML = total
+  numStrHolder.push(parseInt(currentDisplay))
+  isSubtracting = true
+  isAdding = false
+  currentDisplay = ""
+  document.querySelector("#placeToPutResult").innerText = 0
+  console.log(
+    numStrHolder,
+    "numStrHolder",
+    isSubtracting,
+    typeof numStrHolder[0],
+  )
+}
+
+function multiply() {
+  numStrHolder.push(parseInt(currentDisplay))
+  isMultiplying = true
+  isDividing = false
+  currentDisplay = ""
+  document.querySelector("#placeToPutResult").innerText = 0
+  console.log(numStrHolder, "multiply")
+}
+
+function divide() {
+  numStrHolder.push(parseInt(currentDisplay))
+  isMultiplying = false
+  isDividing = true
+  currentDisplay = ""
+  document.querySelector("#placeToPutResult").innerText = 0
+  console.log(numStrHolder, "divide")
+}
+
+function makeZero() {
+  numStrHolder.length = 0
+  currentDisplay = ""
+  total = 0
+  isAdding = false
+  isSubtracting = false
+  document.querySelector("#placeToPutResult").innerText = 0
+}
+
+function displayValue() {
+  currentDisplay += this.innerText
+  document.querySelector("#placeToPutResult").innerText = currentDisplay
+  console.log(currentDisplay)
 }
